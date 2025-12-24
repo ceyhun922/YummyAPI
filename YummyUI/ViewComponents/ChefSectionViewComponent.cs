a4805cbf92c723e41a9c0814b179219e715ccd62
Ceyhun Yusif <jeyhun312@gmail.com>
2025-12-24 17:32:19 +0400

    Chef API si dinamik olaraq UI ye yerleşdi

--- YummyUI/ViewComponents/ChefSectionViewComponent.cs
+++ YummyUI/ViewComponents/ChefSectionViewComponent.cs
@@ -1,12 +1,38 @@
-using Microsoft.AspNetCore.Mvc;
 
-namespace YummyUI.ViewComponents 
+
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YummyUI.DTOs.ChefDTOs;
+
+
+
+namespace YummyUI.ViewComponents
 {
     public class ChefSectionViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ChefSectionViewComponent(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("http://localhost:5289/api/Chefies/");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+
+                var values = JsonConvert.DeserializeObject<List<ResultChefDto>>(jsonData);
+
+                return View(values);
+            }
             return View();
         }
     }
 }
+
+
